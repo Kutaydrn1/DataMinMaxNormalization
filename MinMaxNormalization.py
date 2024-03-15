ad7b804ce7c225c014f96de3c379e9d134832729
Kutay DURAN <152699887+Kutaydrn1@users.noreply.github.com>
2024-03-15 13:49:21 +0300

    Add files via upload

---
+++ MinMaxNormalization.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from urllib.request import urlopen
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+url="https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+veri=pd.read_csv(url, names=["sepal_length","sepal width","petal_length","petal_width","class"])
+print(veri)
+
+X=veri.iloc[:,0:4]
+#data.drop['class', axis=1]
+Y=veri.iloc[:,4]
+#data['class']
+
+scaleFunc=MinMaxScaler(feature_range=(0,1))
+dataScaled=scaleFunc.fit_transform(X)
+
+dataScaled=pd.DataFrame(dataScaled,columns=X.columns)
+print(dataScaled)
